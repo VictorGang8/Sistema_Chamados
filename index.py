d98d52ce08c3d44a19af15c9bf79eeb5d6e7820f
VictorGang8 <victor.vgtogabriel@gmail.com>
2026-05-19 19:27:33 -0300

    Finalizando o código

--- index.py
+++ index.py
@@ -1,23 +1,45 @@
 import streamlit as st
-
 st.title("Sistema de Chamados")
 
-#Criar lista de Chamados
-if "Chamados" not in st.session_state:
+#Criar a lista de chamados
+if "chamados" not in st.session_state:
     st.session_state.chamados = []
-
-#Abrir Chamado    
+    
+#Abrir chamado
 st.subheader("Abrir Chamado")
-titulo = st.text_input("Título do Chamado")
-descricacao = st.text_area("Descrição do Serviço")
+titulo=st.text_input("Título do Chamado")
+descricao=st.text_area("Descrição de serviço")
 
 #Botão
-if st.button("Abrir Chamado"):
-    if titulo != "" and descricacao != "":
+if st.button("Abrir chamado"):
+    if titulo != "" and descricao != "":
         chamado = {
             "titulo": titulo,
-            "descricao": descricacao,
+            "descricao":descricao,
             "status": "Aberto"
         }
         st.session_state.chamados.append(chamado)
-        st.success("Chamado aberto com sucesso")
+        st.success("Chamado aberto com Sucesso")
+
+st.subheader("Lista de Chamados")
+if len(st.session_state.chamados) == 0 :
+    st.warning("Nenhum chamado aberto")
+else:
+    for i,chamado in enumerate(st.session_state.chamados):
+        st.write(f"{chamado['titulo']}")
+        st.write(f"Descrição: {chamado['descricao']}")
+        st.write(f"Status: {chamado['status']}")
+
+        #Alterar status
+        novo_status = st.selectbox(
+            "Alterar Status",
+            ["Aberto", "Em andamento", "Finalizado"],
+            key=f"status{i}"
+        )
+
+        #Botão para alterar
+        if st.button("Atualizar Status", key=f"btn{i}"):
+            st.session_state.chamados[i]["status"] = novo_status
+            st.success("Status Atualizado!")
+            st.rerun()
+        st.divider()
